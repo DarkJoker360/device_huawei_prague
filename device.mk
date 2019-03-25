c6f4899a25b7575614c14f4181235d78d4d6f362
hak86 <hakamelassouad@gmail.com>
2019-03-25 13:39:40 +0100

    prague: copy aosp codecs

--- device.mk
+++ device.mk
@@ -52,6 +52,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:system/etc/primary_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
 
+# Codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Device
 $(call inherit-product, device/huawei/hi6250-common/device.mk)
 
