d87b5600f446aee2dbcd1822cf75674242366e11
DarkJoker360 <simoespo159@gmail.com>
2019-04-18 09:15:07 +0200

    omx: blacklist snapchat from TOPAZ encoder

--- device.mk
+++ device.mk
@@ -65,6 +65,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/media-codec-blacklist:system/etc/media-codec-blacklist
+
 # Device
 #$(call inherit-product, device/huawei/hi6250-common/device.mk)
 
